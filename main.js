d20fb8c40464865737f1877345ebe6a346db7a0b
Phillip <96359059+phillipashford@users.noreply.github.com>
2023-10-01 15:52:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,53 @@
 // import the MapLibre library and our styles
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./style.css";
-import key from "./scripts/key.env";
 import maplibregl from "maplibre-gl";
+import { API_KEY } from './key';
+const apiKey = API_KEY;
 
-//create the map object
+// Create the map object
 const map = new maplibregl.Map({
   container: "map",
   center: [-90, 38],
   zoom: 4,
-  style: `https://api.maptiler.com/maps/outdoor-v2/style.json?key=${key}`,
+  style: `https://api.maptiler.com/maps/outdoor-v2/style.json?key=${apiKey}`,
 });
 
+// Add source data and layers after the map has loaded
+map.on("load", function () {
+  // Add source data
+  map.addSource("counties", {
+    type: "geojson",
+    data: "data/us-counties-optimized.json",
+  });
+  map.addSource("states", {
+    type: "geojson",
+    data: "data/us-states-optimized.json",
+  });
+
+    // Add geojson polygon layers
+    map.addLayer({
+      id: "counties",
+      type: "fill",
+      source: "counties",
+      paint: {
+        "fill-color": "#aaa",
+        "fill-opacity": 0.8,
+      },
+    });
+  
+    map.addLayer({
+      id: "states",
+      type: "line",
+      source: "states",
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+      },
+      paint: {
+        "line-color": "#888",
+        "line-width": 0.4,
+      },
+    });
+
+});
