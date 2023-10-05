584c7e4144fd6d71a2ef27090b50aed626cfe763
Phillip <96359059+phillipashford@users.noreply.github.com>
2023-10-05 07:52:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,51 +2,74 @@
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./style.css";
 import maplibregl from "maplibre-gl";
-import { API_KEY } from './key';
+import { API_KEY } from './scripts/key';
 const apiKey = API_KEY;
 
 // Create the map object
 const map = new maplibregl.Map({
   container: "map",
-  center: [-90, 38],
-  zoom: 4,
+  center: [-82.6422, 27.7650],
+  zoom: 13.75,
   style: `https://api.maptiler.com/maps/outdoor-v2/style.json?key=${apiKey}`,
 });
+
+const openUI = () => {
+  offCanvas.classList.remove("translate-x-[-100%]");
+  overlay.classList.remove("opacity-0");
+  offCanvas.classList.add(
+    "translate-x-0",
+    "transition-transform",
+    "duration-300",
+    "ease-in-out"
+  );
+  overlay.classList.add("opacity-100", "pointer-events-auto");
+};
+
+const closeUI = () => {
+  offCanvas.classList.remove("translate-x-0");
+  overlay.classList.remove("opacity-100", "pointer-events-auto");
+  offCanvas.classList.add("translate-x-[-100%]");
+  overlay.classList.add("opacity-0");
+};
+
+openButton.addEventListener("click", openUI);
+overlay.addEventListener("click", closeUI);
+closeButton.addEventListener("click", closeUI);
 
 // Add source data and layers after the map has loaded
 map.on("load", function () {
   // Add source data
-  map.addSource("counties", {
+  map.addSource("watersheds", {
     type: "geojson",
-    data: "data/us-counties-optimized.json",
+    data: "data/study-area-watersheds.geojson",
   });
-  map.addSource("states", {
+  map.addSource("pinellas-contours", {
     type: "geojson",
-    data: "data/us-states-optimized.json",
+    data: "data/pinellas-study-dem.geojson",
   });
 
     // Add geojson polygon layers
-    map.addLayer({
-      id: "counties",
+  map.addLayer({
+      id: "watersheds",
       type: "fill",
-      source: "counties",
+      source: "watersheds",
       paint: {
         "fill-color": "#aaa",
         "fill-opacity": 0.8,
       },
     });
-  
+
     map.addLayer({
-      id: "states",
+      id: "contours",
       type: "line",
-      source: "states",
+      source: "pinellas-contours",
       layout: {
         "line-join": "round",
         "line-cap": "round",
       },
       paint: {
-        "line-color": "#888",
-        "line-width": 0.4,
+        "line-color": "pink",
+        "line-width": 1,
       },
     });
 
